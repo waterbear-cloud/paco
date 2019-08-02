f3d83d4de77fdfdf0825c13bf04095137bc52e8c
Kevin Lindsay <klindsay@waterbear.cloud>
2019-08-01 22:55:54 -0700

    Added TODO for building SNSTopic Configurations.

--- src/aim/controllers/ctl_notificationgroups.py
+++ src/aim/controllers/ctl_notificationgroups.py
@@ -36,12 +36,15 @@ class NotificationGroupsStackGroup(StackGroup):
     def init(self):
         "init"
         # create a template
+        sns_topics_config = []
+        # TODO: Build a list of ISNSTopic configurations
+        breakpoint()
         ng_sns_template = aim.cftemplates.SNSTopics(
             self.aim_ctx,
             self.account_ctx,
             self.region,
             'NG',
-            self.config,
+            sns_topics_config,
             self.resource_ref
         )
 
