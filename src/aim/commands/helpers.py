f392c1734ab6d96aa4130bf550c696f48e0da57c
Kevin Lindsay <klindsay@waterbear.cloud>
2019-08-13 12:07:25 -0700

    Commend out debug line

--- src/aim/commands/helpers.py
+++ src/aim/commands/helpers.py
@@ -46,7 +46,7 @@ def handle_exceptions(func):
     """
     @wraps(func)
     def decorated(*args, **kwargs):
-        return func(*args, **kwargs)
+        #return func(*args, **kwargs)
         try:
             return func(*args, **kwargs)
         except (InvalidAimReference, UnusedAimProjectField, InvalidAimProjectFile, AimException, StackException, BotoCoreError,
