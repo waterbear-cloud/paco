1077a10e0cc4c294070901005dbf1c3b67b7fdaa
Kevin Teague <kevin.t@waterbear.cloud>
2019-08-24 13:06:38 -0700

    Upgrade cw_alarms to troposphere

--- src/aim/cftemplates/cw_alarms.py
+++ src/aim/cftemplates/cw_alarms.py
@@ -4,6 +4,7 @@ CloudFormation template for CloudWatch Alarms
 
 import aim.models.services
 import json
+import troposphere
 from aim.cftemplates.cftemplates import CFTemplate
 from aim.models import schemas
 from aim.models import vocabulary
@@ -20,7 +21,6 @@ class CWAlarms(CFTemplate):
         aws_region,
         stack_group,
         stack_tags,
-
         alarm_sets,
         res_type,
         res_config_ref,
@@ -41,120 +41,44 @@ class CWAlarms(CFTemplate):
         self.dimension = vocabulary.cloudwatch[res_type]['dimension']
 
         # Define the Template
-        template_fmt = """
-AWSTemplateFormatVersion: '2010-09-09'
-Description: 'CloudWatch Alarms'
+        template = troposphere.Template()
+        template.add_version('2010-09-09')
+        template.add_description('CloudWatch Alarms')
 
-Parameters:
-
-{0[parameters]:s}
-
-Resources:
-
-{0[alarms]:s}
-
-Outputs:
-
-{0[outputs]:s}
-"""
-        template_table = {
-            'parameters': None,
-            'alarms': None,
-            'outputs': None,
-        }
-
-        output_fmt = """
-  Alarm{0[id]:s}:
-    Value: !Ref Alarm{0[id]:s}
-"""
-
-        # Important: If you specify a name, you cannot perform updates that require
-        # replacement of this resource. You can perform updates that require no or
-        # some interruption. If you must replace the resource, specify a new name.
-        # AlarmName: '{0[name]:s}'
-        # ToDo: enable settings for
-        # Metrics :
-        #   - MetricDataQuery
-        # Unit: String
-        # DatapointsToAlarm: Integer
-
-        alarm_fmt = """
-  Alarm{0[id]:s}:
-    Type: AWS::CloudWatch::Alarm
-    Properties:
-{0[alarm_actions]:s}
-      AlarmDescription: '{0[description]:s}'
-      ComparisonOperator: {0[comparison_operator]:s}
-      Dimensions: {0[dimensions]:s}
-      EvaluateLowSampleCountPercentile: {0[evaluate_low_sample_count_percentile]:s}
-      EvaluationPeriods: {0[evaluation_periods]:d}
-      ExtendedStatistic: {0[extended_statistic]:s}
-      MetricName: {0[metric_name]:s}
-      Namespace: {0[namespace]:s}
-      Period: {0[period]:d}
-      Statistic: {0[statistic]:s}
-      Threshold: {0[threshold]:f}
-      TreatMissingData: {0[treat_missing_data]:s}
-"""
-        dimensions_fmt = """
-        - Name: {}
-          Value: {}"""
-
-        alarm_table = {
-            'id': None,
-            'description': None,
-            'name': None,
-            'comparison_operator': None,
-            'dimensions': None,
-            'evaluation_periods': 0,
-            'metric_name': None,
-            'namespace': None,
-            'period': 0,
-            'statistic': None,
-            'threshold': 0,
-            'treat_missing_data': None,
-            'extended_statistic': None,
-            'evaluate_low_sample_count_percentile': None,
-            'alarm_actions': None
-        }
-
-        alarms_yaml = ""
-        outputs_yaml = ""
-        parameters_yaml = ""
-        parameters_yaml += self.gen_parameter(
-            param_type='String',
-            name='DimensionResource',
-            description='The resource id or name for the metric dimension.',
-            value=resource.aim_ref + '.name'
+        # Add Parameters
+        dimension_param = self.gen_parameter(
+            param_type = 'String',
+            name = 'DimensionResource',
+            description = 'The resource id or name for the metric dimension.',
+            value = resource.aim_ref + '.name',
+            use_troposphere = True
         )
+        template.add_parameter(dimension_param)
 
+        # Add Alarm resources
         for alarm_set_id in alarm_sets.keys():
             alarm_set = alarm_sets[alarm_set_id]
             for alarm_id in alarm_set.keys():
                 alarm = alarm_set[alarm_id]
-                notification_arns = [
-                    self.aim_ctx.project['notificationgroups'][group].resource_name for group in alarm.notification_groups
-                ]
-                description = alarm.get_alarm_description(notification_arns)
-                normalized_set_id = self.normalize_resource_name(alarm_set_id)
-                normalized_id = self.normalize_resource_name(alarm_id)
+                alarm_resource_name = 'Alarm{}{}'.format(
+                    self.normalize_resource_name(alarm_set_id),
+                    self.normalize_resource_name(alarm_id)
+                )
 
-                # Alarm actions
-                alarm_actions = alarm.get_alarm_actions(self.aim_ctx.project['notificationgroups'])
-                if len(alarm_actions) > 0 and alarm_actions[0] != None and alarm_actions[0] != '':
-                    alarm_actions_cfn = "      ActionsEnabled: True\n      AlarmActions:\n"
-                    for action in alarm_actions:
-                        alarm_actions_cfn += "         - " + action
-                else:
-                    alarm_actions_cfn = '      ActionsEnabled: False\n'
+                # compute dynamic attributes for cfn_export_dict
+                alarm_export_dict = alarm.cfn_export_dict
+
+                # Namespace - if not supplied default to the Namespace for the Resource type
+                if 'Namespace' not in alarm_export_dict:
+                    alarm_export_dict['Namespace'] = vocabulary.cloudwatch[res_type]['namespace']
 
                 # Dimensions
                 # if there are no dimensions, then fallback to the default of
                 # a primary dimension and the resource's resource_name
-
                 if len(alarm.dimensions) < 1:
                     dimensions = [
-                        (vocabulary.cloudwatch[res_type]['dimension'], '!Ref DimensionResource')
+                        {'Name': vocabulary.cloudwatch[res_type]['dimension'],
+                         'Value': troposphere.Ref(dimension_param)}
                     ]
                 else:
                     dimensions = []
@@ -165,52 +89,24 @@ Outputs:
                         else:
                             value = dimension.value
                         dimensions.append(
-                            (dimension.name, value)
+                            {'Name': dimension.name, 'Value': value}
                         )
-                dimensions_str = ''
-                for name, value in dimensions:
-                    #if value == None or value == '':
-                    #    breakpoint()
-                    dimensions_str += dimensions_fmt.format(name, value)
+                alarm_export_dict['Dimensions'] = dimensions
 
-                # Metric Namespace can override default Resource Namespace
-                if alarm.namespace:
-                    namespace = alarm.namespace
-                else:
-                    namespace = vocabulary.cloudwatch[res_type]['namespace']
+                # Add Alarm resource
+                alarm_resource = troposphere.cloudwatch.Alarm.from_dict(
+                    alarm_resource_name,
+                    alarm_export_dict
+                )
+                template.add_resource(alarm_resource)
 
-                alarm_table['alarm_actions'] = alarm_actions_cfn
-                alarm_table['id'] = normalized_set_id+normalized_id
-                alarm_table['description'] = description
-                alarm_table['name'] = alarm_id
-                alarm_table['comparison_operator'] = alarm.comparison_operator
-                alarm_table['evaluation_periods'] = alarm.evaluation_periods
-                alarm_table['namespace'] = namespace
-                alarm_table['dimensions'] = dimensions_str
-                alarm_table['evaluation_periods'] = alarm.evaluation_periods
-                alarm_table['metric_name'] = alarm.metric_name
-                alarm_table['period'] = alarm.period
-                alarm_table['threshold'] = alarm.threshold
-                alarm_table['treat_missing_data'] = alarm.treat_missing_data
-                if alarm.extended_statistic == None:
-                    alarm_table['statistic'] = alarm.statistic
-                    alarm_table['extended_statistic'] = "!Ref AWS::NoValue"
-                else:
-                    alarm_table['statistic'] = '!Ref AWS::NoValue'
-                    alarm_table['extended_statistic'] = alarm.extended_statistic
-                if alarm.evaluate_low_sample_count_percentile == None:
-                    alarm_table['evaluate_low_sample_count_percentile'] = "!Ref AWS::NoValue"
-                else:
-                    alarm_table['evaluate_low_sample_count_percentile'] = alarm.evaluate_low_sample_count_percentile
-
-                alarms_yaml += alarm_fmt.format(alarm_table)
-                outputs_yaml += output_fmt.format(alarm_table)
+                # Alarm Output
                 output_ref = '.'.join([res_config_ref, 'monitoring', 'alarm_sets', alarm_set_id, alarm_id])
-                self.register_stack_output_config(output_ref, 'Alarm'+alarm_table['id'])
+                self.register_stack_output_config(output_ref, alarm_resource_name)
+                alarm_output = troposphere.Output(
+                    alarm_resource_name,
+                    Value=troposphere.Ref(alarm_resource)
+                )
+                template.add_output(alarm_output)
 
-        template_table['parameters'] = parameters_yaml
-        template_table['alarms'] = alarms_yaml
-        template_table['outputs'] = outputs_yaml
-
-        self.set_template(template_fmt.format(template_table))
-
+        self.set_template(template.to_yaml())
