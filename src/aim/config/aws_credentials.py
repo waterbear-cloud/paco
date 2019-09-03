5d94d3ef85ce6510d6a0f8cb8fd98ed121f81f2d
Kevin Lindsay <klindsay@waterbear.cloud>
2019-09-03 15:53:23 -0700

    Testing what could be long term credentials.

--- src/aim/config/aws_credentials.py
+++ src/aim/config/aws_credentials.py
@@ -25,13 +25,14 @@ class Sts(object):
                 ):
         self.sts_token_expiry_seconds = 3500 # Limited to 1 hour until we use
         self.temp_creds_path = temporary_credentials_path
-        self.credentails = None
+        self.credentials = None
         self.mfa_arn = mfa_arn
         self.account_ctx = account_ctx
         self.mfa_account = mfa_account
         self.admin_creds = admin_creds
         self.admin_iam_role_arn = admin_iam_role_arn
         self.session = None
+        self.sts_client = boto3.client('sts')
 
 
     def get_temporary_credentials(self):
@@ -58,25 +59,44 @@ class Sts(object):
         except (IOError, ClientError):
             response = None
             if self.admin_creds != None:
-                session = boto3.Session(aws_access_key_id=self.admin_creds.aws_access_key_id,
-                                        aws_secret_access_key=self.admin_creds.aws_secret_access_key,
-                                        region_name=self.admin_creds.aws_default_region)
+                session = boto3.Session(
+                    region_name=self.admin_creds.aws_default_region,
+                    aws_access_key_id=self.admin_creds.aws_access_key_id,
+                    aws_secret_access_key=self.admin_creds.aws_secret_access_key,
+                )
+
                 token_code = input('MFA Token: {0}: '.format(self.account_ctx.get_name()))
-                response = session.client('sts').assume_role(
-                    DurationSeconds=self.sts_token_expiry_seconds,
+                sts_client = session.client('sts')
+                response = sts_client.get_session_token(
+                    DurationSeconds=(60*60)*6, # 6 hours
+                )
+                credentials = response['Credentials']
+                session = boto3.Session(
+                    aws_access_key_id=credentials['AccessKeyId'],
+                    aws_secret_access_key=credentials['SecretAccessKey'],
+                    aws_session_token=credentials['SessionToken'],
+                    region_name=self.admin_creds.aws_default_region,
+                )
+
+                sts_client = session.client('sts')
+
+                response = sts_client.assume_role(
+                    DurationSeconds=60*15, # 15 minues
                     RoleArn=self.admin_iam_role_arn,
                     RoleSessionName='aim-multiaccount-session',
-                    SerialNumber=self.mfa_arn,
-                    TokenCode=token_code)
+                    TokenCode=token_code,
+                    SerialNumber=self.mfa_arn
+                )
             elif self.mfa_account != None:
                 mfa_creds = self.mfa_account.get_temporary_credentials()
                 session = boto3.Session(aws_access_key_id=mfa_creds['AccessKeyId'],
                                         aws_secret_access_key=mfa_creds['SecretAccessKey'],
                                         aws_session_token=mfa_creds['SessionToken'])
                 response = session.client('sts').assume_role(
-                    DurationSeconds=self.sts_token_expiry_seconds,
+                    DurationSeconds=60*15,
                     RoleArn=self.admin_iam_role_arn,
                     RoleSessionName='aim-multiaccount-session')
+            # Cache the credentials
             credentials = response['Credentials']
             with open(self.temp_creds_path, 'w') as tmp_creds:
                 tmp_creds.write(json.dumps({
