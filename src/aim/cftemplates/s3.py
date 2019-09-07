45cfd7fd52a942af5ed202d28ac9b1e1170bb957
Kevin Teague <kevin.t@waterbear.cloud>
2019-09-07 10:27:46 +0300

    S3 is now troposphere

--- src/aim/cftemplates/s3.py
+++ src/aim/cftemplates/s3.py
@@ -1,7 +1,12 @@
+import awacs.s3
 import os
+import troposphere
+import troposphere.cloudfront
+import troposphere.s3
 from aim.cftemplates.cftemplates import CFTemplate
 from aim.cftemplates.cftemplates import Parameter
 from aim.cftemplates.cftemplates import StackOutputParam
+from awacs.aws import Allow, Statement, Policy, Principal
 from io import StringIO
 from enum import Enum
 
@@ -39,216 +44,131 @@ class S3(CFTemplate):
 
         self.s3_context_id = config_ref
         self.bucket_context = bucket_context
-
-        # Define the Template
-        template_fmt = """
-AWSTemplateFormatVersion: '2010-09-09'
-Description: 'S3 Buckets'
-
-#Parameters:
-#{0[parameters_yaml]:s}
-
-Resources:
-{0[resources_yaml]:s}
-
-# Dummy Resource
-  DummyResource:
-    Type: AWS::CloudFormation::WaitConditionHandle
-
-{0[outputs_yaml]:s}
-"""
-        template_table = {
-            'parameters_yaml': "",
-            'resources_yaml': "",
-            'outputs_yaml': ""
-        }
-
-        s3_bucket_fmt = """
-  {0[cf_resource_name_prefix]:s}Bucket:
-    Type: AWS::S3::Bucket
-    Properties:
-      BucketName: {0[bucket_name]:s}
-    DeletionPolicy: Retain
-"""
-
-        cloudfront_origin_fmt = """
-  CloudFrontOriginAccessIdentity:
-    Type: AWS::CloudFront::CloudFrontOriginAccessIdentity
-    Properties:
-      CloudFrontOriginAccessIdentityConfig:
-        Comment: '{0[access_id_description]:s}'
-
-  CloudFrontBucketPolicy:
-    Type: AWS::S3::BucketPolicy
-    DependsOn:
-      - CloudFrontOriginAccessIdentity
-      - {0[cf_resource_name_prefix]:s}Bucket
-    Properties:
-      Bucket: {0[bucket_name]:s}
-      PolicyDocument:
-        Statement:
-          - Effect: Allow
-            Principal:
-              CanonicalUser: !GetAtt CloudFrontOriginAccessIdentity.S3CanonicalUserId
-            Action:
-              - s3:GetObject
-            Resource:
-              - arn:aws:s3:::{0[bucket_name]:s}/*
-"""
-        cloudfront_origin_table = {
-            'access_id_description': None,
-            'cf_resource_name_prefix': None,
-            'bucket_name': None
-        }
-
-        s3_policy_fmt = """
-  {0[cf_resource_name_prefix]:s}BucketPolicy:
-  {0[cf_policy_depends_on]:s}
-    Type: AWS::S3::BucketPolicy
-    Properties:
-      Bucket: {0[bucket_name]:s}
-      PolicyDocument:
-        Statement: {0[policy_statements]:s}
-"""
-        s3_policy_depends_on_fmt = """  DependsOn:
-      - {0[cf_resource_name_prefix]:s}Bucket"""
-
-        s3_policy_table = {
-            'cf_resource_name_prefix': "",
-            'bucket_name' : "",
-            'policy_statements': "",
-            'cf_policy_depends_on': ""
-        }
-
-        s3_policy_statement_fmt = """
-          - Action:{0[action_list]:s}
-            Effect: {0[effect]:s}
-            Principal:
-{0[principal]:s}
-            Resource: {0[resource_list]:s}
-            {0[condition]:s}
-"""
-        s3_policy_statement_table = {
-            'action_list': "",
-            'effect': "",
-            'principal': "",
-            'resource_list': "",
-            'condition': "",
-            }
-
-        s3_bucket_outputs_fmt = """
-  {0[cf_resource_name_prefix]:s}BucketName:
-    Value: !Ref {0[cf_resource_name_prefix]:s}Bucket
-"""
-
-        s3_bucket_table = {
-            'bucket_name': None,
-            'cf_resource_name_prefix': None
-        }
-
-        cf_list_item = """
-              - '{0}'"""
-
-        cf_principal_list_item = """
-                - '{0}'"""
-
-        parameters_yaml = ""
-        resources_yaml = ""
-        outputs_yaml = ""
-
+        bucket = bucket_context['config']
         s3_ctl = self.aim_ctx.get_controller('S3')
-
-        s3_bucket_table.clear()
-        bucket_config = bucket_context['config']
-        s3_bucket_table['bucket_name'] = s3_ctl.get_bucket_name(self.s3_context_id)
+        bucket_name = s3_ctl.get_bucket_name(self.s3_context_id)
         cf_name_prefix = self.gen_cf_logical_name(self.bucket_context['id'], '_')
-        s3_bucket_table['cf_resource_name_prefix'] = cf_name_prefix
 
+        # ---------------------------------------------------------------------------
+        # Troposphere Template Initialization
+        template = troposphere.Template(
+            Description=bucket.title_or_name
+        )
+        template.set_version()
+
+        # ---------------------------------------------------------------------------
+        # Resources
         if bucket_policy_only == False:
-            # parameters_yaml += s3_bucket_params_fmt.format(s3_bucket_table)
-            resources_yaml += s3_bucket_fmt.format(s3_bucket_table)
+            s3_logical_id = self.gen_cf_logical_name(self.bucket_context['id'], '_') + 'Bucket'
+            cfn_export_dict = bucket.cfn_export_dict
+            cfn_export_dict['BucketName'] = bucket_name
+            s3_resource = troposphere.s3.Bucket.from_dict(s3_logical_id, cfn_export_dict)
+            template.add_resource(s3_resource)
+            troposphere.add_output(
+                troposphere.Output(
+                    s3_logical_id,
+                    Value=troposphere.Ref(s3_resource)
+                )
+            )
+            self.register_stack_output_config(config_ref + '.name', cf_name_prefix + 'BucketName')
 
-        if bucket_config.cloudfront_origin == True:
-            cloudfront_origin_table['bucket_name'] = s3_bucket_table['bucket_name']
-            cloudfront_origin_table['cf_resource_name_prefix'] = cf_name_prefix
-            cloudfront_origin_table['access_id_description'] = self.s3_context_id
-            resources_yaml += cloudfront_origin_fmt.format(cloudfront_origin_table)
-            outputs_yaml += self.gen_output('CloudFrontOriginAccessIdentity', '!Ref CloudFrontOriginAccessIdentity')
+        if bucket.cloudfront_origin == True:
+            # CloudFront OriginAccessIdentity resource
+            cloudfront_origin_resource = troposphere.cloudfront.CloudFrontOriginAccessIdentity.from_dict(
+                'CloudFrontOriginAccessIdentity',
+                {'CloudFrontOriginAccessIdentityConfig': {'Comment': self.s3_context_id}},
+            )
+            template.add_resource(cloudfront_origin_resource)
+
+            # S3 BucketPolicy resource
+            policy = Policy(
+                Version='2012-10-17',
+                Statement=[
+                    Statement(
+                        Effect = Allow,
+                        Principal = Principal('CanonicalUser',troposphere.GetAtt('CloudFrontOriginAccessIdentity','S3CanonicalUserId')),
+                        Action = [awacs.s3.GetObject],
+                        Resource = ['arn:aws:s3:::{}/*'.format(bucket_name)],
+                    )
+                ]
+            )
+            bucket_policy_resource = troposphere.s3.BucketPolicy(
+                'CloudFrontBucketPolicy',
+                Bucket = bucket_name,
+                PolicyDocument = policy,
+            )
+            bucket_policy_resource.DependsOn = [
+                'CloudFrontOriginAccessIdentity',
+                s3_logical_id
+            ]
+            template.add_output(
+                troposphere.Output(
+                    'CloudFrontOriginAccessIdentity',
+                    Value=troposphere.Ref(cloudfront_origin_resource)
+                )
+            )
+            template.add_resource(bucket_policy_resource)
+
+            # Output CloudFrontOriginAccessIdentity
             self.register_stack_output_config(config_ref+'.origin_id', 'CloudFrontOriginAccessIdentity')
-        elif len(bucket_config.policy) > 0:
+        elif len(bucket.policy) > 0:
             # Bucket Policy
-            if bucket_policy_only == False:
-                s3_policy_table['cf_policy_depends_on'] = s3_policy_depends_on_fmt.format(s3_bucket_table)
+            # ToDo: allow mixing CloudFront Origin policies and other bucket policies together
 
-            s3_policy_table['cf_resource_name_prefix'] = cf_name_prefix
-            s3_policy_table['bucket_name'] = s3_bucket_table['bucket_name']
-            s3_policy_table['policy_statements'] = ""
             # Statement
-            for policy_statement in bucket_config.policy:
+            statements = []
+            for policy_statement in bucket.policy:
                 if policy_statement.processed == True:
                     continue
-                s3_policy_statement_table['action_list'] = ""
-                s3_policy_statement_table['principal'] = ""
-                s3_policy_statement_table['effect'] = ""
-                s3_policy_statement_table['resource_list'] = ""
-                # Action
-                for action in policy_statement.action:
-                    s3_policy_statement_table['action_list'] += cf_list_item.format(action)
-                # Effect
-                s3_policy_statement_table['effect'] = policy_statement.effect
+                statement_dict = {
+                    'Effect': policy_statement.effect,
+                    'Action': [
+                        Action(*action.split(':')) for action in policy_statement.actions
+                    ],
+                }
 
-                # Principal - principal field
+                # Principal
                 if policy_statement.principal != None and len(policy_statement.principal) > 0:
+                    # ToDo: awacs only allows one type of Principal ... is there a use-case where
+                    # multiple principal types are needed?
                     for key, value in policy_statement.principal.items():
-                        s3_policy_statement_table['principal'] = "              {}:".format(key)
-                        # can be a string or list
-                        if type(value) == type(list()):
-                            for item in value:
-                                s3_policy_statement_table['principal'] += cf_principal_list_item.format(item)
-                        else:
-                            s3_policy_statement_table['principal'] += cf_principal_list_item.format(value)
-
-                # Principal - aws field
+                        statement_dict['Principal'] = Principal(key, value)
                 elif policy_statement.aws != None and len(policy_statement.aws) > 0:
-                    s3_policy_statement_table['principal'] = "              AWS:"
-                    for principal in policy_statement.aws:
-                        s3_policy_statement_table['principal'] += cf_principal_list_item.format(principal)
+                    statement_dict['Principal'] = Principal('AWS', policy_statement.aws)
 
+                # Condition
                 if policy_statement.condition != {}:
-                    padding = "              "
-                    condition = "Condition:\n"
-                    for key, value in policy_statement.condition.items():
-                        condition += padding + "  {}:\n".format(key)
-                        for sub_key, sub_value in value.items():
-                            if type(sub_value) == type(list()):
-                                condition += padding + "    '{}':\n".format(sub_key)
-                                for item in sub_value:
-                                    condition += padding + "      - '{}'\n".format(item)
-                            else:
-                                condition += padding + "    '{}': '{}'\n".format(sub_key, sub_value)
-                    s3_policy_statement_table['condition'] = condition
+                    statement_dict['Condition'] = policy_statement.condition
 
                 # Resource
                 bucket_arn = s3_ctl.get_bucket_arn(self.s3_context_id)
                 if policy_statement.resource_suffix and len(policy_statement.resource_suffix) > 0:
-                    for res_suffix in policy_statement.resource_suffix:
-                        resource_arn = bucket_arn + res_suffix
-                        s3_policy_statement_table['resource_list'] += cf_list_item.format(resource_arn)
+                    statement_dict['Resource'] = [
+                        bucket_arn + res_suffix
+                        for res_suffix in policy_statement.resource_suffix
+                    ]
                 else:
-                    s3_policy_statement_table['resource_list'] = cf_list_item.format(bucket_arn)
-                s3_policy_table['policy_statements'] += s3_policy_statement_fmt.format(s3_policy_statement_table)
-            resources_yaml += s3_policy_fmt.format(s3_policy_table)
-        if bucket_policy_only == False:
-            outputs_yaml += s3_bucket_outputs_fmt.format(s3_bucket_table)
-            self.register_stack_output_config(config_ref+'.name', cf_name_prefix+'BucketName')
+                    statement_dict['Resource'] = [bucket_arn]
 
-        template_table['parameters_yaml'] = parameters_yaml
-        template_table['resources_yaml'] = resources_yaml
-        if outputs_yaml != '':
-            outputs_yaml = "Outputs:\n"+outputs_yaml
-        template_table['outputs_yaml'] = outputs_yaml
+                statements.append(
+                    Statement(**statement_dict)
+                )
 
-        self.set_template(template_fmt.format(template_table))
+            bucket_policy_resource = troposphere.s3.BucketPolicy(
+                cf_name_prefix + 'BucketPolicy',
+                Bucket = bucket_name,
+                PolicyDocument = Policy(
+                    Version = '2012-10-17',
+                    Statement = statements,
+                ),
+            )
+
+            if bucket_policy_only == False:
+                bucket_policy_resource.DependsOn = s3_resource
+
+        # Generate the Template
+        self.set_template(template.to_yaml())
+
 
     def delete(self):
         s3_ctl = self.aim_ctx.get_controller('S3')
