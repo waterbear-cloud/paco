d423b8b04e0e1640703bdcb2f9e754073867fe91
Kevin Teague <kevin.t@waterbear.cloud>
2019-11-20 13:20:50 +0200

    delete cmd clean-up

--- src/aim/commands/cmd_delete.py
+++ src/aim/commands/cmd_delete.py
@@ -1,41 +1,61 @@
 import aim.models
 import click
 import sys
-from aim.commands.helpers import pass_aim_context, controller_args, aim_home_option, init_aim_home_option, handle_exceptions
+from aim.commands.helpers import (
+    controller_args, aim_home_option, init_aim_home_option, pass_aim_context,
+    handle_exceptions, cloud_options, set_cloud_options, cloud_args, config_types
+)
 from aim.core.exception import StackException
+from aim.models.references import get_model_obj_from_ref
 
 
 @click.command('delete', short_help='Delete AIM managed resources')
-@controller_args
 @aim_home_option
+@cloud_args
+@cloud_options
 @pass_aim_context
 @handle_exceptions
-def delete_command(aim_ctx, controller_type, arg_1=None, arg_2=None, arg_3=None, arg_4=None, home='.'):
+def delete_command(
+    aim_ctx,
+    verbose,
+    nocache,
+    yes,
+    disable_validation,
+    quiet_changes_only,
+    config_type,
+    config_scope,
+    home='.'
+):
     """Deletes provisioned AWS Resources"""
-    aim_ctx.command = 'delete'
-    init_aim_home_option(aim_ctx, home)
-    if not aim_ctx.home:
-        print('AIM configuration directory needs to be specified with either --home or AIM_HOME environment variable.')
-        sys.exit()
-
-    delete_name = "{0} {1}".format(controller_type, arg_1)
-    if arg_2:
-        delete_name += " {0}".format(arg_2)
-
-    answer = aim_ctx.input_confirm_action("Proceed with deletion?")
+    controller_type, controller_args = set_cloud_options(
+        'delete',
+        aim_ctx,
+        verbose,
+        nocache,
+        yes,
+        disable_validation,
+        quiet_changes_only,
+        config_type,
+        config_scope,
+        home
+    )
+    aim_ref = 'aim.ref {}.{}'.format(config_type, config_scope)
+    obj = get_model_obj_from_ref(aim_ref, aim_ctx.project)
+    print('Resource selected for deletion:')
+    print('  Type: {}'.format(obj.__class__.__name__))
+    print('  Name: {}'.format(obj.name))
+    if getattr(obj, 'title', None):
+        print('  Title: {}'.format(obj.title))
+    print('  Reference: {}'.format(obj.aim_ref))
+    answer = aim_ctx.input_confirm_action("Proceed with deletion of {} {}?".format(config_type, config_scope))
     if answer == False:
         print("Aborted delete operation.")
         return
 
-    aim_ctx.log("Delete: Controller: {}  arg_1({}) arg_2({}) arg_3({}) arg_4({})".format(controller_type, arg_1, arg_2, arg_3, arg_4) )
-    aim_ctx.load_project()
-
-    controller_args = {
-        'command': 'delete',
-        'arg_1': arg_1,
-        'arg_2': arg_2,
-        'arg_3': arg_3,
-        'arg_4': arg_4
-    }
     controller = aim_ctx.get_controller(controller_type, controller_args)
     controller.delete()
+
+delete_command.help = """
+Delete cloud resources.
+
+""" + config_types
