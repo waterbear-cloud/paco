a27d9affc2bb25003f3f5090dc797fb4e38be6a7
Kevin Teague <kevin.t@waterbear.cloud>
2020-06-17 13:35:43 +0300

    add ECR Pull permissions

--- src/paco/application/ec2_launch_manager.py
+++ src/paco/application/ec2_launch_manager.py
@@ -1456,6 +1456,18 @@ statement:
       - 'ecs:Submit*'
       - 'logs:CreateLogStream'
       - 'logs:PutLogEvents'
+      - 'ecr:GetAuthorizationToken'
+      - 'ecr:BatchCheckLayerAvailability'
+      - 'ecr:GetDownloadUrlForLayer'
+      - 'ecr:GetRepositoryPolicy'
+      - 'ecr:DescribeRepositories'
+      - 'ecr:ListImages'
+      - 'ecr:DescribeImages'
+      - 'ecr:BatchGetImage'
+      - 'ecr:GetLifecyclePolicy'
+      - 'ecr:GetLifecyclePolicyPreview'
+      - 'ecr:ListTagsForResource'
+      - 'ecr:DescribeImageScanFindings'
     resource:
       - '*'
 """
