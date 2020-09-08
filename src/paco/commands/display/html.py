38bd0a03be4e65281d90868687637f42130b2e89
Kevin Teague <kevin.t@waterbear.cloud>
2020-09-08 11:30:00 +0300

    continue work on describe

--- src/paco/commands/display/html.py
+++ src/paco/commands/display/html.py
@@ -1,20 +1,67 @@
+
+from paco.models.references import get_model_obj_from_ref
 import os
 import pathlib
 from chameleon import PageTemplateLoader
-from paco.models.references import get_model_obj_from_ref
+import chameleon.loader
 
+# Generic Resource templates
+# if 'res-<resource.type>.pt' does not exist, the template will return the generic 'res-generic.pt'.
+# Chameleon needs a big ugly monkey patch on the TemplateLoader.load() method to achieve this ...
+def generic_resource_load(self, spec, cls=None):
+    if cls is None:
+        raise ValueError("Unbound template loader.")
+
+    spec = spec.strip()
+
+    if self.default_extension is not None and '.' not in spec:
+        spec += self.default_extension
+
+    if ':' in spec:
+        spec = chameleon.loader.abspath_from_asset_spec(spec)
+
+    if not os.path.isabs(spec):
+        for path in self.search_path:
+            path = os.path.join(path, spec)
+            if os.path.exists(path):
+                spec = path
+                break
+        else:
+            if spec.startswith('res-'):
+                spec = os.path.join(self.search_path[0], 'res-generic.pt')
+            else:
+                raise ValueError("Template not found: %s." % spec)
+
+    return cls(spec, search_path=self.search_path, **self.kwargs)
+chameleon.loader.TemplateLoader.load = generic_resource_load
+
+
+def has_alarms(res):
+    if getattr(res, 'monitoring', None) != None:
+        if getattr(res.monitoring, 'alarm_sets', None) != None:
+            if len(res.monitoring.alarm_sets) > 0:
+                return True
+    return False
+
+def has_logs(res):
+    if getattr(res, 'monitoring', None) != None:
+        if getattr(res.monitoring, 'log_sets', None) != None:
+            if len(res.monitoring.log_sets) > 0:
+                return True
+    return False
 
 def display_project_as_html(project):
     path = os.path.dirname(__file__)
     static_path = pathlib.Path(path) / 'static'
     templates = PageTemplateLoader(path)
+    templates.registry
     userinfo = project.credentials
 
     def resolve_ref(ref_string):
         return get_model_obj_from_ref(ref_string, project)
 
     html_files = {}
-    for tmpl in ['index.pt', 'accounts.pt', 'globalresources.pt', 'netenvs.pt']:
+    for tmpl in ['index.pt', 'accounts.pt', 'globalresources.pt', 'netenvs.pt', 'monitoring.pt']:
         fname = tmpl[:-2] + 'html'
         html_files[fname] = templates[tmpl](
             project=project,
@@ -34,6 +81,8 @@ def display_project_as_html(project):
                 env=env,
                 userinfo=userinfo,
                 templates=templates,
+                has_alarms=has_alarms,
+                has_logs=has_logs,
             )
 
     return static_path, html_files, envs_html
