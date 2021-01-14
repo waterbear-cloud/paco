a5d3b57b7e56058bdf41292af5e9f53dad071421
Kevin Teague <kevin.t@waterbear.cloud>
2021-01-13 17:23:39 -0800

    Preparing release 9.2.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     keywords=['AWS','Waterbear','Cloud','Infrastructure as Code', 'CloudFormation'],
     install_requires=[
-        'paco.models >= 7.7.3',
+        'paco.models >= 7.7.4',
         'boto3 >= 1.16.48',
         'click',
         'cookiecutter',
